82ecf673d717350aec999010029c818a8f607c17
MaevaPrecourt <maeva.precourt@gmail.com>
2023-03-08 18:43:28 +0100

    Ajouts complémentaires - 8/3

--- create_react_app/src/App.js
+++ create_react_app/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
+function App(){
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,6 +20,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
